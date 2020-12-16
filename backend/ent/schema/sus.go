ea3ee5b607e4eeb39b899bde9d0ed9a40b8bc623
Haiserest <69398619+Haiserest@users.noreply.github.com>
2020-12-17 01:55:10 +0700

    create sus -close #7

---
+++ backend/ent/schema/sus.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Sus holds the schema definition for the Sus entity.
+type Sus struct {
+	ent.Schema
+}
+
+// Fields of the Sus.
+func (Sus) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Sus.
+func (Sus) Edges() []ent.Edge {
+	return nil
+}
