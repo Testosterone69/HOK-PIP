1ceafbcd1d171789417f598c8c4bda6b745fdb6c
Haiserest <69398619+Haiserest@users.noreply.github.com>
2020-12-21 23:49:02 +0700

    plus heeted -close #6

--- backend/ent/schema/heeted.go
+++ backend/ent/schema/heeted.go
@@ -1,16 +1,22 @@
 package schema
 
-import "github.com/facebookincubator/ent"
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+ )
 
 // Heeted holds the schema definition for the Heeted entity.
 type Heeted struct {
 	ent.Schema
 }
 
-// Fields of the Heeted.
-func (Heeted) Fields() []ent.Field {
-	return nil
-}
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("age").Positive(),
+		field.String("name").NotEmpty(),
+	}
+ }
 
 // Edges of the Heeted.
 func (Heeted) Edges() []ent.Edge {
